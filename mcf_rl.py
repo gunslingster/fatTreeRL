7b5eefe021b0ae1d2bab67c0127b3d39f203b65d
sterlingabrahams@yahoo.com <sterlingabrahams@yahoo.com>
2022-06-30 23:51:06 -0700

    Find shortest path via Q learning in fat tree network

--- mcf_rl.py
+++ mcf_rl.py
@@ -50,9 +50,6 @@ def genNetwork(k, weight_function, capacity_function):
 	for node in flow_network.nodes:
 		print(node)
 		print(flow_network[node])
-	print(f"Source: {source}")
-	print(f"Dest: {dest}")
-	print(f"edges{edges}")
 	return source, dest, flow_network
 
 # Initialize Q matrix according to step 1
@@ -63,6 +60,7 @@ def initQ(G, node_lookup, index_lookup):
 	for node1 in G.nodes:
 		for node2 in G[node1]:
 			Q[node_lookup[node1], node_lookup[node2]] = 0
+			Q[node_lookup[node2], node_lookup[node1]] = 0
 	print(pd.DataFrame(data=Q, index=[node for node in G.nodes], columns=[node for node in G.nodes]))
 	return Q
 
@@ -103,21 +101,50 @@ def chooseNextNode(G, Q, node_lookup, index_lookup, current_node, thresh):
 	next_node = random.choice(possible_actions)
 	return next_node
 
-def updateQ(node1, node2, Q, R, node_lookup, index_lookup lr, discount):
-	pass
+def updateQ(G, current_node, next_node, Q, R, node_lookup, index_lookup, lr, discount):
+	# Here we have a current node and next node
+	# We are going to update the Q matrix according to to the standard Q update method
+	# We combine the current Q value and the expected future reward in proportion to the lr
+	# First we need to calculate the quality of being in the new state
+	max_value = np.max(Q[node_lookup[next_node]])
+	Q[node_lookup[current_node], node_lookup[next_node]] = \
+	(1 - lr) * Q[node_lookup[current_node], node_lookup[next_node]] + \
+	lr * (R[node_lookup[current_node], node_lookup[next_node]] + \
+	discount * max_value)	
+
+def learn(G, Q, R, node_lookup, index_lookup, thresh, lr, discount):
+	for i in range(50000):
+		source = random.choice(list(G.nodes))
+		next_node = chooseNextNode(G, Q, node_lookup, index_lookup, source, thresh)
+		updateQ(G, source, next_node, Q, R, node_lookup, index_lookup, lr, discount)
+	print(pd.DataFrame(data=Q, index=[node for node in G.nodes], columns=[node for node in G.nodes]))
+
+def shortestPath(G, source, dest, node_lookup, index_lookup, Q,):
+	path = [source]
+	next_node = index_lookup[np.argmax(Q[node_lookup[source],])]
+	path.append(next_node)
+	while next_node != dest:
+		next_node = index_lookup[np.argmax(Q[node_lookup[next_node],])]
+		path.append(next_node)
+	print(path)
+	return path
 
 def main():
-	k = 2
+	k = 4
 	capacity_function = lambda _: random.randint(1,10)
 	weight_function = lambda _: random.randint(1,10)
-	source, dest, network = genNetwork(k, weight_function, capacity_function)
+	source, dest, G = genNetwork(k, weight_function, capacity_function)
+	thresh = 0.2
+	lr = 0.8
+	discount = 0.8
 	print(f"Source: {source}")
 	print(f"Dest: {dest}")
-	node_lookup, index_lookup = nodeLookupTable(network.nodes)
+	node_lookup, index_lookup = nodeLookupTable(G.nodes)
 	print(f"Node lookup table: {node_lookup}")
-	Q = initQ(network, node_lookup, index_lookup)
-	R = initR(network, dest, node_lookup, index_lookup, 100)
-	chooseNextNode(network, Q, node_lookup, index_lookup, 100, 0.5)
+	Q = initQ(G, node_lookup, index_lookup)
+	R = initR(G, dest, node_lookup, index_lookup, 100)
+	learn(G, Q, R, node_lookup, index_lookup, thresh, lr, discount)
+	shortestPath(G, source, dest, node_lookup, index_lookup, Q)
 
 if __name__ == "__main__":
 	main()
